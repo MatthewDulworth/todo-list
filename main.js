e1cdea4c7473cc1a762dc9281c0c3708a70b38d6
Matthew Dulworth <mhdulworth@gmail.com>
2019-08-13 15:05:40 -0500

    dumb local storage that doesn't work well added

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+let counter = 0;
+init();
 
 // show the create new todo menu
 $("#plus").click(function () {
@@ -5,13 +7,7 @@ $("#plus").click(function () {
    $("#create-todo").toggleClass("visible");
 });
 
-/******************************** this bit is relevent ***************************/
-// the problem is the icons flash white for a second on mouse-exit instead of transitioning smoothly like they do on mouse-enter
-// hover effect for icons
-// $("#plus, .fa-trash",).hover(function () {
-//    $(this).toggleClass("rainbow-text");
-// });
-
+// hover effects
 $("main").on({
    mouseenter: function () {
       $(this).addClass("rainbow-text");
@@ -29,11 +25,19 @@ $(".rotate").click(function () {
 // create new todo
 $("#create-todo").keypress(function (event) {
    if (event.which === 13) {
-      let todoTextContent = $(this).val();
-      $("ul").prepend(`<li><div><span class='check far fa-circle'></span><input class='todo-text' value="${todoTextContent}" maxlength="50" autocomplete="off"></div><span class='menu'><i class='fa fa-trash'></i></span></li>`
-      );
+      let html = `<li id="${counter}"><div><span class='check far fa-circle'></span><input class='todo-text' value="${$(this).val()}" maxlength="50" autocomplete="off"></div><span class='menu'><i class='fa fa-trash'></i></span></li>`;
+      $("ul").prepend(html);
       $(this).val("");
+      localStorage.setItem(counter,html);
+      counter++;
+      localStorage.setItem("counter",counter);
    }
+});
+
+// clear button 
+$("#clear").click(function(){
+   $("ul").empty();
+   localStorage.clear();
 });
 
 // checkmarks
@@ -43,5 +47,20 @@ $("ul").on("click", ".check", function () {
 
 // delete todo
 $("ul").on("click", ".fa-trash", function () {
+   localStorage.removeItem($(this).parent().parent().attr("id"));
    $(this).parent().parent().remove();
 });
+
+// local storage 
+function init(){
+   for(i=localStorage.getItem("counter"); i>=0; i--){
+      $("ul").append(localStorage.getItem(i));
+   }
+}
+
+// clear function
+function clearTodo(){
+   $("ul").empty();
+   localStorage.clear();
+   counter = 0;
+}
